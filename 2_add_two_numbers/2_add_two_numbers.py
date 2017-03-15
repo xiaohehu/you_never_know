1c90e7707f25860bec170d19cf0bb0a31cf43b30
Xiaohe Hu <xiaohehu@dhcp-wifi-8021x-155-41-15-144.bu.edu>
2017-03-15 14:15:58 -0400

    2_add_two_numbers

--- 2_add_two_numbers/2_add_two_numbers.py
+++ 2_add_two_numbers/2_add_two_numbers.py
@@ -11,3 +11,21 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        result = ListNode(0)
+        node = result
+        extra = 0
+        while l1 or l2 or extra:
+            if l1 == None:
+                l1 = ListNode(0)
+            if l2 == None:
+                l2 = ListNode(0)
+                
+            node.next = ListNode((l1.val + l2.val + extra) % 10)
+            extra = (l1.val + l2.val + extra) // 10
+            node = node.next
+            l1 = l1.next
+            l2 = l2.next
+        
+        return result.next
+                
+            
