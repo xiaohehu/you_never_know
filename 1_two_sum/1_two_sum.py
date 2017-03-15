adcfe9e2cb642b7c79240f3493f02f8b7dc7d0f6
Xiaohe Hu <xiaohehu@Xiaohes-Air.fios-router.home>
2017-03-14 23:57:31 -0400

    Finished two sum in python

--- 1_two_sum/1_two_sum.py
+++ 1_two_sum/1_two_sum.py
@@ -7,3 +7,23 @@ class Solution(object):
 		:type target: int
 		:rtype: List[int]
 		"""
+		result = []
+		hashMap = {}
+		i = 0
+		# Step 1 generate a dictionary, key is number, value is index
+		while i < len(nums):
+			hashMap[nums[i]] = i
+			i += 1
+		# Go through the list find dictionary has the number
+		for item in nums:
+			if hashMap.has_key(target - item):
+				# In case same number reused 
+				if nums.index(item) != hashMap[target - item]:
+					result.append(nums.index(item))
+					result.append(hashMap[target - item])
+					break
+		return result
+
+haha = Solution()
+print(haha.twoSum([3,2,4], 6))
+		
