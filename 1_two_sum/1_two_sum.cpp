a55cc13dfaf6422431e1812bf6f244959486d0b3
Xiaohe Hu <huxiaohe113@gmail.com>
2017-04-05 16:55:00 -0400

    Added 1. Two Sum cpp answer

---
+++ 1_two_sum/1_two_sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            if (hash.find(target - nums[i]) != hash.end()){
+                ans.push_back(hash[target - nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            hash[nums[i]] = i;
+        }
+        return ans;
+    }
+};
