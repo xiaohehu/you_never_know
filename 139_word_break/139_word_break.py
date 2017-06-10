4b66f26e83e0bf43877fe724990dd7ebec95893d
Xiaohe <huxiaohe113@gmail.com>
2017-06-10 18:26:46 -0400

    Added 139 Word Break

---
+++ 139_word_break/139_word_break.py
@@ -0,0 +1,15 @@
+class Solution(object):
+	def wordBreak(self, s, wordDict):
+		"""
+		:type s: str
+		:type wordDict: Set[str]
+		:rtype: bool
+		"""
+		dp = [False] * (len(s) + 1) # dp[i] means s[:i+1] can be segmented into words in the wordDicts 
+		dp[0] = True
+		for i in range(len(s)):
+			for j in range(i, len(s)):
+				if dp[i] and s[i: j+1] in wordDict:
+					dp[j+1] = True
+					
+		return dp[-1]
